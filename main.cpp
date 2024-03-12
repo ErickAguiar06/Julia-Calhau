37a71fa3ae69ec7762034fbecc1291e4028459e1
ErickAguiar06 <160655450+ErickAguiar06@users.noreply.github.com>
2024-03-12 19:11:37 -0300

    Time de futebol

--- main.cpp
+++ main.cpp
@@ -1,15 +1,20 @@
 #include <stdio.h>
 int main()
 {
-	int x, y, z;
-    printf("Digite o primeiro numero x:");
-    scanf("%d" ,&x);
-
-    printf("Digite o segundo numero y:");
-    scanf("%d" ,&y);
-    z = (x +y);
+    char nome_time[50];
+    int numero_vitorias, numero_empates, total_pontos;
+    printf("Digite o nome do time de futebol: ");
+    scanf("%49s", nome_time);
     
-    printf("A soma de x + y é igual a = ");
-	printf("%d" ,z);
-	return 0;
+    printf("Digite o número de vitórias:");
+    scanf("%d", &numero_vitorias);
+    
+    printf("Digite o número de empates:");
+    scanf("%d", &numero_empates);
+    
+    total_pontos = (numero_vitorias * 3) + numero_empates;
+    
+    printf("O time %s tem um total de %d pontos.\n", nome_time,total_pontos);
+    
+    return 0;
 }
